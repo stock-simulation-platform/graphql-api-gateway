aae8e1d7ab56b25d9a9b0f394e6a413d2f2f726c
Truong Thanh Vinh <69314040+thanhvinh941@users.noreply.github.com>
2025-05-30 20:11:06 +0700

    init source graphql-api-gateway

---
+++ src/main/java/com/stocksim/gateway/GraphqlApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.stocksim.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stocksim/gateway/GraphqlApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocksim.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
